057cf68425c9edab891a6b4009d0c7730f446f92
adbras <michelcerqueira12@gmail.com>
2015-07-09 23:31:07 -0300

    primeiro comit nova versao

---
+++ js/roulette.js
@@ -0,0 +1,22 @@
+var $r = $('.roulette').fortune(240);
+	
+	var x = 0;	
+	
+
+var clickHandler = function() {
+  $('.spinner').off('click');
+  $('.spinner span').hide();
+  $r.spin().done(function(price) {
+      $('.spinner').on('click', clickHandler);
+      $('.spinner span').show();
+	   window.localStorage.clickcount = Number(localStorage.clickcount) + 1;
+	
+		x+=1;
+ 
+    
+        
+	   
+    });
+};
+
+$('.spinner').on('click', clickHandler);
